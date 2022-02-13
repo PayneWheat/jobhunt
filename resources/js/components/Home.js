2941618bae150c78297b32032c2afdf59f966b8f
Payne <wheatp5@gmail.com>
2022-02-12 21:08:02 -0600

    Breeze components

--- resources/js/components/Home.js
+++ resources/js/components/Home.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
+import Authenticated from '@/Layouts/Authenticated';
 import ApplicationsList from './ApplicationsList';
 import InterviewsList from './InterviewsList';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 class Home extends Component {
     render() {
         return (
-            <div className='container'>
-                <ApplicationsList />
-                
-                <h2 className='jh-heading'>Interviews</h2>
-                <InterviewsList />
-            </div>
+            <Authenticated
+                auth={props.auth}
+                errors={props.errors}
+                header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
+            >
+                <Head title="Home" />
+
+                <div className='container'>
+                    <ApplicationsList />
+                    
+                    <h2 className='jh-heading'>Interviews</h2>
+                    <InterviewsList />
+                </div>
+            </Authenticated>
         );
     }
 }
